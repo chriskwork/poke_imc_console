ea566a99331fee07a4f8306f51a97ed29ee28886
chris k <yovirinc@gmail.com>
2025-02-02 00:03:04 +0100

    Added pokemon_stats table

--- lib/database/db_service.dart
+++ lib/database/db_service.dart
@@ -15,6 +15,8 @@ class DatabaseService {
     );
 
     // Intentar a Crear el BD si no existe.
+    // Primnero se conecta con setting temporal,
+    // y luego crea el BD que vamos a gardar datos.
     try {
       final tempConn = await MySqlConnection.connect(tempSettings);
       await tempConn.query('CREATE DATABASE IF NOT EXISTS pokemon_imc');
@@ -23,8 +25,16 @@ class DatabaseService {
       // Conectar a BD de la App.
       conn = await MySqlConnection.connect(DatabaseConfig.settings);
 
-      // Crear la tabla [trainer(user)]
-      await conn.query('''
+      // Iniciar tablas
+      _initialTables();
+    } catch (e) {
+      print('Error: Problema con el BD, $e');
+    }
+  }
+
+  Future<void> _initialTables() async {
+    // Crear la tabla [trainer(user)]
+    await conn.query('''
         CREATE TABLE IF NOT EXISTS trainer (
         trainer_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
         username varchar(50) NOT NULL,
@@ -33,30 +43,40 @@ class DatabaseService {
         created_at timestamp DEFAULT CURRENT_TIMESTAMP
       )''');
 
-      // Crear la tabla [imc]
-      await conn.query('''
+    // Crear la tabla [imc]
+    await conn.query('''
         CREATE TABLE IF NOT EXISTS imc (
         record_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
         trainer_id int NOT NULL,
         height decimal(5,2) NOT NULL,
         weight decimal(5,2) NOT NULL,
         imc decimal(3,1) NOT NULL,
-        recorded_at timestamp DEFAULT CURRENT_TIMESTAMP
+        recorded_at timestamp DEFAULT CURRENT_TIMESTAMP,
         FOREIGN KEY (trainer_id) REFERENCES trainer(trainer_id)
       )''');
 
-      // Crear la tabla [user_pokemon]
-      await conn.query('''
+    // Crear la tabla [user_pokemon]
+    await conn.query('''
         CREATE TABLE IF NOT EXISTS user_pokemon (
         record_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        user_id int NOT NULL,
+        trainer_id int NOT NULL,
         pokemon_id int NOT NULL,
         pokemon_name varchar(50) NOT NULL,
-        obtained_at timestamp DEFAULT CURRENT_TIMESTAMP
-        FOREIGN KEY (user_id) REFERENCES trainer(user_id)
+        obtained_at timestamp DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (trainer_id) REFERENCES trainer(trainer_id),
+        FOREIGN KEY (pokemon_id) REFERENCES pokemon_stats(pokemon_id)
       )''');
-    } catch (e) {
-      print('Error: Problema con el BD, $e');
-    }
+
+    // Crear la tabla [pokemon_stats]
+    await conn.query('''
+        CREATE TABLE IF NOT EXISTS pokemon_stats (
+        pokemon_id int NOT NULL PRIMARY KEY,
+        trainer_id int NOT NULL,
+        pokemon_name varchar(50) NOT NULL,
+        height decimal(5,2) NOT NULL,
+        weight decimal(5,2) NOT NULL,
+        imc decimal(3,1) NOT NULL,
+        
+      )''');
   }
 }
